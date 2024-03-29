72da8cb52b1447b9573f2cbd1c0ba8485e621342
Alex <alexander.k.kostadinov@gmail.com>
2024-03-29 02:10:28 +0200

    Added category creation front-end validation.

--- src/main/resources/static/js/add-category-controller.js
+++ src/main/resources/static/js/add-category-controller.js
@@ -1,15 +1,42 @@
 const categoryAction = document.querySelector(".add-category .add-category__actions button");
 const categoryName = document.querySelector(".add-category #categoryName");
+let isCreateCategoryCancelled = false;
 
 categoryAction.addEventListener("click", createCategory);
 
-categoryName.addEventListener("keypress", function(evt) {
-	if(evt.key === "Enter" && evt.keyCode === 13) {
-		createCategory();
+/**
+ * Front-End Validation
+ */
+categoryName.addEventListener("keyup", function(evt) {
+	const categoryNameError = categoryName.nextElementSibling;
+	const categoryNameRegex = /^[A-Z](?:[a-zA-Z]\s?){1,19}$/;
+
+	if(!categoryNameRegex.test(categoryName.value)) {
+		categoryName.classList.add("is-invalid");
+		categoryNameError.innerText = "Please enter a category containing from 2 to 20 letters.\nEach new word should start with a capital letter.";
+
+		isCreateCategoryCancelled = true;
+	} else {
+		categoryName.classList.remove("is-invalid");
+		categoryNameError.innerText = "";
+		isCreateCategoryCancelled = false;
+
+		if(evt.key === "Enter" && evt.keyCode === 13) {
+			createCategory();
+		}
 	}
 });
 
+/**
+ * Back-End Validation
+ */
 function createCategory() {
+	if(isCreateCategoryCancelled) {
+		return;
+	}
+
+	categoryName.value = categoryName.value.trim();
+
 	const requestOptions = {
 		method: "POST",
 		headers: {
