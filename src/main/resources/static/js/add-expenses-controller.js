76ef42b0eb735b12045ad7770633aba686ef9bd5
Alex <alexander.k.kostadinov@gmail.com>
2024-03-31 02:27:46 +0200

    Added transaction creation front-end validation.

--- src/main/resources/static/js/add-expenses-controller.js
+++ src/main/resources/static/js/add-expenses-controller.js
@@ -1,5 +1,37 @@
 const expenseInput = document.querySelector(".add-expenses #expense");
+const expenseCategory = document.querySelector(".add-expenses #category");
 const expenseControls = document.querySelectorAll(".add-expenses__control");
+let isCreateTransactionCancelled = false;
+
+expenseInput.addEventListener("keyup", function(evt) {
+	const expenseError = expenseInput.nextElementSibling;
+	const expenseRegex = /^[1-9]\d{0,6}$/;
+
+	if(!expenseRegex.test(expenseInput.value)) {
+		expenseInput.classList.add("is-invalid");
+		expenseError.innerText = "Please enter a positive expense amount.\nMaximum length is considered to be 7 digits.";
+
+		isCreateTransactionCancelled = true;
+	} else {
+		expenseInput.classList.remove("is-invalid");
+		expenseError.innerText = "";
+		isCreateTransactionCancelled = false;
+
+		if(evt.key === "Enter" && evt.keyCode === 13) {
+			createTransaction();
+		}
+	}
+});
+
+expenseCategory.addEventListener("change", function(evt) {
+	if(this.options[this.selectedIndex].value === "" || this.selectedIndex === 0) {
+		this.classList.add("is-invalid");
+		this.nextElementSibling.innerText = "Please choose a category";
+	} else {
+		this.classList.remove("is-invalid");
+		this.nextElementSibling.innerText = "";
+	}
+});
 
 /**
  * Create transaction
@@ -7,6 +39,12 @@ const expenseControls = document.querySelectorAll(".add-expenses__control");
 addExpensesBtn.addEventListener("click", createTransaction);
 
 function createTransaction() {
+	if(isCreateTransactionCancelled) {
+		return;
+	}
+
+	expenseInput.value = expenseInput.value.trim();
+
 	const requestOptions = {
 		method: "POST",
 		headers: {
