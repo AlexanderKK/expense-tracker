c4924877d3e649a6499c892e67a19bba316e50d2
DarkFirePhoenixX <s.slavkov20@gmail.com>
2024-04-27 19:18:55 +0300

    Updated Front-End

--- src/main/resources/static/js/icon-picker.js
+++ src/main/resources/static/js/icon-picker.js
@@ -1,6 +1,6 @@
 const dropdown = document.querySelector(".dropdown");
-const input = document.querySelector(".dropdown input");
-const listOfOptions = document.querySelectorAll(".option");
+const input = document.getElementById("iconSelect");
+const listOfOptions = document.querySelectorAll(".icon-option");
 
 const body = document.body;
 const toggleDropdown = (event) => {
@@ -26,10 +26,14 @@ listOfOptions.forEach((option) => {
 
 dropdown.addEventListener('click', toggleDropdown);
 
-document.querySelectorAll(".option").forEach((e) => {
+document.querySelectorAll(".icon-option").forEach((e) => {
+	if(e === undefined){}
+	else{
 	e.addEventListener("click", () => {
-		document.getElementById("iconSelect").setAttribute("placeholder", e.firstChild.className.split(" ")[2])
-		document.getElementById("iconPicked").innerHTML = "Selected Icon:" + e.outerHTML
+		// input.setAttribute("placeholder", e.firstChild.className.split(" ")[2])
+		document.querySelector(".selected-icon").innerHTML = "Selected icon:" + e.outerHTML
+		document.querySelector(".selected-icon").setAttribute("data-icon",e.classList)
 		console.log(e)
 	})
+}
 })
